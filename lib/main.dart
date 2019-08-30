e145cf9abe1ebd960674de9fdafc5daa9b92caba
Hrishikesh Mahajan <mahajanhm4@gmail.com>
2019-08-30 16:34:06 +0530

    No graph

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'donut.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 void main() {
   runApp(MyApp());
@@ -14,52 +15,253 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   List<Widget> _widgetOptions = <Widget>[
-    
-    Container( //Code for dashboard begins here
+    Container(
+      //Code for dashboard begins here
       decoration: BoxDecoration(
         gradient: LinearGradient(colors: [
-          const Color(0xFF3F51B5),
-          const Color(0xFF2196F3),
+          const Color(0xFF1B5E20),
+          const Color(0xFF66BB6A),
         ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
       ),
       child: ListView(
         padding: EdgeInsets.all(10.0),
-
         children: <Widget>[
           //Insert dashboard Code here
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.white30,
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                border: Border.all(
+                width: 2,
+                ),
+            ),
+            child: Center(
+                child: Text('Scope Wise Distribution',
+                    style: TextStyle(color: Colors.white, fontSize: 24))),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white30,
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  border: Border.all(
+                  width: 2,
+                  ),
+              ),
+              child: Container(
+                child: Center(
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        width: 200,
+                        height: 250,
+                        child: DonutAutoLabelChart.withSampleData(),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+
+            padding: const EdgeInsets.all(5.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white30,
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                border: Border.all(
+                  width: 2,
+                ),
+              ),
+              child: Center(
+                  child: Text('Worst performing scope: Scope 2',style: TextStyle(color: Colors.white, fontSize: 25),)
+              ),
+            ),
+          ),
+          Row(children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(1.0),
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text(
+                        "Scope 1",
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Text(
+                      "35",
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Container(
+                      child: Text(
+                        "Good",
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      color: Colors.green,
+                    )
+                  ],
+                ),
+                height: 100,
+                width: 127,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  color: Colors.white30,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text(
+                        "Scope 2",
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Text(
+                      "55",
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Container(
+                      child: Text(
+                        "Moderate",
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      color: Colors.yellow[600],
+                    )
+                  ],
+                ),
+                height: 100,
+                width: 127,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  color: Colors.white30,
+                  border: Border.all(
+                    width: 2,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Text(
+                        "Scope 3",
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Text(
+                      "45",
+                      style: TextStyle(
+                        fontSize: 22,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Container(
+                      child: Text(
+                        "Good",
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      color: Colors.green,
+                    )
+                  ],
+                ),
+                height: 100,
+                width: 127,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  color: Colors.white30,
+                ),
+              ),
+            ),
+          ]),
         ],
       ),
     ),
-    Container( //Code for graphs begins here
+    Container(
+      //Code for graphs begins here
       decoration: BoxDecoration(
         gradient: LinearGradient(colors: [
-          const Color(0xFF3F51B5),
-          const Color(0xFF2196F3),
+          const Color(0xFF1B5E20),
+          const Color(0xFF66BB6A),
         ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
       ),
       child: ListView(
         padding: EdgeInsets.all(10.0),
-
         children: <Widget>[
           //Insert Graph Code here
+          Container(
+            width: 200,
+            height: 250,
+          )
         ],
       ),
     ),
-    Container(  //Code for legend begins here
+    Container(
+      //Code for legend begins here
       decoration: BoxDecoration(
         gradient: LinearGradient(colors: [
-          const Color(0xFF3F51B5),
-          const Color(0xFF2196F3),
+          const Color(0xFF1B5E20),
+          const Color(0xFF66BB6A),
         ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
       ),
       child: ListView(
         padding: EdgeInsets.all(10.0),
-
         children: <Widget>[
           Container(
             decoration: BoxDecoration(
@@ -72,8 +274,7 @@ class _MyAppState extends State<MyApp> {
                   alignment: Alignment.topCenter,
                   child: Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child:
-                    Column(
+                    child: Column(
                       children: <Widget>[
                         Text(
                           'Scope 1',
@@ -108,12 +309,10 @@ class _MyAppState extends State<MyApp> {
                 ),
                 Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child:
-                    Text(
+                    child: Text(
                       "Direct GHG emissions occur from sources that are owned or controlled by the institution, for example, emissions from cooking gas, combustion in owned or controlled boilers, vehicles, etc.; emissions from chemical, pharmaceutical labs.",
                       style: TextStyle(color: Colors.white),
-                    )
-                ),
+                    )),
               ],
             ),
           ),
@@ -130,8 +329,7 @@ class _MyAppState extends State<MyApp> {
                     alignment: Alignment.topCenter,
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child:
-                      Column(
+                      child: Column(
                         children: <Widget>[
                           Text(
                             'Scope 2',
@@ -166,12 +364,10 @@ class _MyAppState extends State<MyApp> {
                   ),
                   Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child:
-                      Text(
+                      child: Text(
                         "This scope accounts for GHG emissions from the generation of purchased electricity consumed by a institution. Purchased electricity is defined as electricity that is purchased or otherwise brought into the organizational boundary of the Institute. These emissions physically occur at the facility where electricity is generated.",
                         style: TextStyle(color: Colors.white),
-                      )
-                  ),
+                      )),
                 ],
               ),
             ),
@@ -189,8 +385,7 @@ class _MyAppState extends State<MyApp> {
                     alignment: Alignment.topCenter,
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child:
-                      Column(
+                      child: Column(
                         children: <Widget>[
                           Text(
                             'Scope 3',
@@ -217,8 +412,7 @@ class _MyAppState extends State<MyApp> {
                   ),
                   Padding(
                     padding: const EdgeInsets.all(8.0),
-                    child:
-                    new Image.asset(
+                    child: new Image.asset(
                       'assets/newScope3.PNG',
                       width: 240,
                       height: 240,
@@ -226,12 +420,10 @@ class _MyAppState extends State<MyApp> {
                   ),
                   Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child:
-                      Text(
+                      child: Text(
                         "This is an optional reporting category that allows for the treatment of all other indirect emissions. These emissions are a consequence of the activities of the Institute, but occur from sources not owned or controlled by the Institute. Some examples of scope 3 activities are extraction and production of purchased materials; transportation of purchased fuels; and use of products and services.",
                         style: TextStyle(color: Colors.white),
-                      )
-                  ),
+                      )),
                 ],
               ),
             ),
@@ -239,7 +431,6 @@ class _MyAppState extends State<MyApp> {
         ],
       ),
     ),
-
   ];
 
   void _onItemTapped(int index) {
@@ -253,37 +444,79 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue[400],
+        backgroundColor: Colors.green,
         leading: Padding(
           padding: const EdgeInsets.all(5.0),
-          child: new Image.asset('assets/sccn_logo.png', fit: BoxFit.cover, color: Colors.white,),
+          child: new Image.asset(
+            'assets/sccn_logo.png',
+            fit: BoxFit.cover,
+            color: Colors.white,
+          ),
         ),
         title: Text('Smart Campus Cloud Network'),
       ),
-      body:Center(
+      body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
-          bottomNavigationBar: BottomNavigationBar(
-            backgroundColor: Colors.blue[400],
-              items: const <BottomNavigationBarItem>[
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.home,color: Colors.white,),
-                  title: Text('Home', style: TextStyle(color: Colors.white),),
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.trending_up,color: Colors.white,),
-                  title: Text('Graphs',style: TextStyle(color: Colors.white),),
-
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(Icons.grid_on, color: Colors.white,),
-                  title: Text('Legend',style: TextStyle(color: Colors.white),),
-                ),
-              ],
-            currentIndex: _selectedIndex,
-            selectedItemColor: Colors.amber[800],
-            onTap: _onItemTapped,
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.green,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Colors.white,
+            ),
+            title: Text(
+              'Home',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.trending_up,
+              color: Colors.white,
+            ),
+            title: Text(
+              'Graphs',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.grid_on,
+              color: Colors.white,
+            ),
+            title: Text(
+              'Legend',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
     ));
   }
+}
+
+class Pollution {
+  String place;
+  int quantity;
+
+  Pollution(this.place, this.quantity);
+}
+
+class Vehicles {
+  int month;
+  double emissions;
+
+  Vehicles(this.month, this.emissions);
+}
+
+class Electricity {
+  String emission;
+  double emission_value;
+
+  Electricity(this.emission, this.emission_value);
 }
